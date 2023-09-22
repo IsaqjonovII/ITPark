73525ffe917de22ae31492cc44e6aacbdd06cb9d
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-09-23 02:09:33 +0500

    working

--- src/interfaces/routes.ts
+++ src/interfaces/routes.ts
@@ -3,6 +3,6 @@ import React from "react";
 export interface IRoutes {
   key: string | number;
   path: string;
-  Component: React.JSXElementConstructor<React.FC> | React.ComponentType<any>;
+  Component: React.ComponentType<any>;
   title?: string;
 }
