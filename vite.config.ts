218b7db24251b9a109f69fce4d1bafbf6b7723eb
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-09-27 12:21:42 +0500

    images optimized

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }]
-  }
-})
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  assetsInclude: /\.(png|jpe?g|gif|svg)$/i,
+});
