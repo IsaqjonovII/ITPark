da8f7df3ab68200e7a4f2a81e4606c4652ecb227
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-09-27 08:55:49 +0500

    working

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
-import ITParkLogo from "./ITP_logo.png";
+import LOGO from "./logo.png";
 import BG from "./bg.svg";
 import BG2 from "./body-bg.png";
-import logoGif from "../../public/favicon.gif";
+import logoGif from "/public/favicon.gif";
 
-export { ITParkLogo, BG, BG2, logoGif };
+export { LOGO, BG, BG2, logoGif };
