0273a7233afb35213ef04a8a0c587b21baab827b
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-10-03 09:35:07 +0500

    workingn

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,6 @@ import LOGO from "./logo.png";
 import nodata from "./nodata.png";
 import notfound from "./notfound.svg";
 import residentsBG from "./residents.svg";
+import logoGif  from "/fav.gif"
 
-export { LOGO, nodata, notfound, residentsBG };
+export { LOGO, nodata, notfound, residentsBG, logoGif };
--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,22 +1,19 @@
 import { useState } from "react";
 import StyledLoader from "./style";
 import LazyImage from "components/LazyImage";
-import { LOGO } from "assets";
+import { logoGif } from "assets";
 
 const Loader = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   setTimeout(() => {
     setIsLoading(false);
-  }, 900);
+  }, 1000);
 
   return (
     <StyledLoader>
       <div className={`loader ${isLoading ? "loading" : "not__loading"}`}>
-        <div className="loader__wrp">
-          <LazyImage className="loader__img" src={LOGO} />
-          <h1>IT Park</h1>
-        </div>
+        <LazyImage src={logoGif} />
       </div>
     </StyledLoader>
   );
