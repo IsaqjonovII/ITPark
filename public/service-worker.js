82ca2b08a3e69731ea818de3aa69661f11477fef
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-10-03 10:40:32 +0500

    pwa

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+     caches.open('airhorner').then(cache => {
+         return cache.addAll([
+             '/',
+             '/?utm_source=homescreen',
+             '/Home/About',
+             '/Home/Index',
+             '/Home/Contact'
+         ])
+             .then(() => self.skipWaiting());
+     })
+   )
+ });
+ 
+ self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+ });
+ 
+ self.addEventListener('fetch', event => {
+   event.respondWith(
+     caches.match(event.request).then(response => {
+         return response || fetch(event.request);
+     })
+   );
+ });
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import GlobalStyles from "styles";
-import Loader from "components/Loader/index.tsx";
+import Loader from "components/Loader";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Error registering Service Worker:", error);
+    });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
