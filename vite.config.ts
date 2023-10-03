2404e2cc4cb9b1df0d995a4bc8115789dd84cb8e
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-10-03 11:19:01 +0500

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), compression()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
-  }
+  },
 });
