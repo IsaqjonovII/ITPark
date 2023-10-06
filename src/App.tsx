4037db269f04b156f08cb9c3db1aec3e11b65be9
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-10-06 12:29:57 +0500

    checking responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { appRoutes } from "routes";
-import Loader from "components/Loader";
+// import Loader from "components/Loader";
 import Footer from "components/Footer";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
@@ -18,19 +18,19 @@ function App() {
         isSidebarOpen={isSidebarOpen}
         setIsSidebarOpen={setisSidebarOpen}
       />  
-      {isSidebarOpen && (
+      {isSidebarOpen ? (
         <Sidebar
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setisSidebarOpen}
         />
-      )}
+      ) : null}
       <Routes>
         {appRoutes.map(({ key, path, Component }) => (
           <Route key={key} path={path} element={<Component />} />
         ))}
       </Routes>
       <Footer />
-      <Loader />
+      {/* <Loader /> */}
     </div>
   );
 }
