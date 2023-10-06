f75da3744a188192d0935a7bdbc1a7832ec9fc37
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2023-10-07 02:05:39 +0500

    sssh happened

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,7 @@ import notfound from "./notfound.svg";
 import residentsBG from "./residents.svg";
 import logoMP4 from "./itplogo.mp4";
 import logo2 from "./logoITpark.png";
+import logo from "./logo.png";
+import banner from "./banner.jpg";
 
-export { nodata, notfound, residentsBG, logo2, logoMP4 };
+export { nodata, notfound, residentsBG, logo2, logoMP4, logo, banner };
